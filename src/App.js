7920bf01bd7deb43d6f9be9a4b49e8a920166b30
Abdulrahman Awad <abdh.3wad@gmail.com>
2024-04-07 15:24:39 +0200

    after dynamic component maps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import Map from "./components/Maps";
+let mapData = {
+  longitude:31.233334,
+  latitude: 30.033333,
+  zoom:7,
+  markers:[
+    {longitude: 50, latitude: 30,},
+    {longitude: 30, latitude: 10,},
+    {longitude: 50, latitude: 20,},
+    {longitude: 31.233334, latitude: 30.033333,},
+  ]
+}
 function App() {
   return (
     <div className="App">
-      <Map/>
+      <Map {...mapData}/>
     </div>
   );
 }
--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,25 +1,20 @@
 import React from "react";
 import Map, { Marker } from "react-map-gl";
-let markersData = [
-  {longitude: 50, latitude: 30,},
-  {longitude: 30, latitude: 10,},
-  {longitude: 50, latitude: 20,},
-]
-function Maps() {
 
+function Maps(props) {
   return (
     <Map
       mapboxAccessToken="pk.eyJ1IjoiM2JkbzN3YWQiLCJhIjoiY2x1aGV0OXBjMm9pNjJrcXV5bTNxa3AzeiJ9.XIMT6_4aR3jy_cBDjss6xA"
       mapLib={import("mapbox-gl")}
       initialViewState={{
-        longitude: 31.233334,
-        latitude: 30.033333,
-        zoom: 7,
+        longitude:props.longitude ,
+        latitude: props.latitude,
+        zoom:props.zoom,
       }}
       style={{ width: 600, height: 400 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-      {markersData.map((m,index)=>(
+      {props.markers.map((m,index)=>(
       <Marker
         key={index}
         longitude={m.longitude}
